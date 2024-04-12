aa478c77db87e23cd824d0aeec48c75669b2dcd8
RoMango <112846459+FlewToL@users.noreply.github.com>
2024-04-12 10:24:36 +0300

    Add files via upload

--- src/exchanger/main/views.py
+++ src/exchanger/main/views.py
@@ -1,11 +1,12 @@
+import json
+
+import yfinance as yf
+from django.http import JsonResponse
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from loguru import logger
 from rest_framework import status, views
 from rest_framework.response import Response
-from drf_yasg.utils import swagger_auto_schema
-from drf_yasg import openapi
-import yfinance as yf
-import json
-from django.http import JsonResponse
-from loguru import logger
 
 
 class CurrencyQuoteAPIView(views.APIView):
@@ -42,30 +43,31 @@ class CurrencyQuoteAPIView(views.APIView):
         period: str = request.headers.get('period')
         interval: str = request.headers.get('interval')
 
+        data = {
+            'currency_pair': currency_pair,
+            'period': period,
+            'interval': interval,
+        }
+
         if not all([currency_pair, period, interval]):
             return Response({"error": "Отсутствует один из обязательных заголовков: currency_pair, period, interval."},
                             status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            ticker = yf.Ticker(currency_pair.upper()+'=X')
+            ticker = yf.Ticker(currency_pair.upper() + '=X')
             hist = ticker.history(period=period, interval=interval)
+            hist.reset_index(inplace=True)
             currency = ticker.info['currency']
             name = ticker.info['longName']
-            last_row = hist.iloc[-1].to_dict()  # Преобразуем последнюю запись в словарь
-            last_row['Name'] = name
-            last_row['Currency'] = currency
+            json_data = hist.to_json(orient='records', date_format='iso')
+            data_list = json.loads(json_data)
 
-            new_currency_pair = currency_pair[3:] + currency_pair[:3]
-
-            ticker = yf.Ticker(new_currency_pair.upper())
-            hist = ticker.history(period=period, interval=interval)
-            currency = ticker.info['currency']
-            name = ticker.info['longName']
-            new_last_row = hist.iloc[-1].to_dict()  # Преобразуем последнюю запись в словарь
-            new_last_row['Name'] = name
-            new_last_row['Currency'] = currency
-
-            end_dict = {f'{currency_pair}': last_row, f'{new_currency_pair}': new_last_row}
+            end_dict = {'info': {
+                'name': name,
+                'currency': currency
+            },
+                'data': data_list
+            }
 
             return JsonResponse(end_dict)
         except Exception as e:
@@ -115,14 +117,18 @@ class CryptoCurrencyQuoteAPIView(views.APIView):
 
             ticker = yf.Ticker(crypto_currency_pair)
             hist = ticker.history(period=period, interval=interval)
+            hist.reset_index(inplace=True)
             currency = ticker.info['currency']
             name = ticker.info['longName']
-            last_row = hist.iloc[-1].to_dict()  # Преобразуем последнюю запись в словарь
-            last_row['Name'] = name
-            last_row['Currency'] = currency
+            json_data = hist.to_json(orient='records', date_format='iso')
+            data_list = json.loads(json_data)
 
-            end_dict = {f'{crypto_currency_pair}': last_row}
-
+            end_dict = {'info': {
+                'name': name,
+                'currency': currency
+            },
+                'data': data_list
+            }
             return JsonResponse(end_dict)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -170,13 +176,92 @@ class StockQuoteAPIView(views.APIView):
 
             ticker = yf.Ticker(stock)
             hist = ticker.history(period=period, interval=interval)
+            hist.reset_index(inplace=True)
             currency = ticker.info['currency']
             name = ticker.info['longName']
-            last_row = hist.iloc[-1].to_dict()  # Преобразуем последнюю запись в словарь
-            last_row['Name'] = name
-            last_row['Currency'] = currency
+            json_data = hist.to_json(orient='records', date_format='iso')
+            data_list = json.loads(json_data)
 
-            end_dict = {f'{stock}': last_row}
+            end_dict = {'info': {
+                'name': name,
+                'currency': currency
+            },
+                'data': data_list
+            }
             return JsonResponse(end_dict)
+        except Exception as e:
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+class TestAPIView(views.APIView):
+    @logger.catch()
+    @swagger_auto_schema(
+        manual_parameters=[
+            openapi.Parameter(
+                name='currency-pair',
+                in_=openapi.IN_HEADER,
+                description="Валюта-Валюта",
+                type=openapi.TYPE_STRING,
+                required=True
+            ),
+            openapi.Parameter(
+                name='period',
+                in_=openapi.IN_HEADER,
+                description="Период - длительность диапазона. "
+                            "К примеру 30d означает от текщуего дня до этого же дня месяц назад.",
+                type=openapi.TYPE_STRING,
+                required=True
+            ),
+            openapi.Parameter(
+                name='interval',
+                in_=openapi.IN_HEADER,
+                description="Интервал - ограничение на внутридневные данные. "
+                            "К примеру период в 30d с интервалом 5d выведет 6 записей "
+                            "с разницей в 5 дней сежду каждой.",
+                type=openapi.TYPE_STRING,
+                required=True
+            ),
+        ]
+    )
+    def get(self, request, *args, **kwargs):
+
+        logger.debug(request.body)
+        logger.debug(request.headers)
+
+        currency_pair: str = request.headers.get('currency-pair')
+        period: str = request.headers.get('period')
+        interval: str = request.headers.get('interval')
+
+        data = {
+            'currency_pair': currency_pair,
+            'period': period,
+            'interval': interval,
+        }
+
+        if not all([currency_pair, period, interval]):
+            return Response({"error": "Отсутствует один из обязательных заголовков: currency_pair, period, interval."},
+                            status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            ticker = yf.Ticker(currency_pair.upper() + '=X')
+            hist = ticker.history(period=period, interval=interval)
+            hist.reset_index(inplace=True)
+            currency = ticker.info['currency']
+            name = ticker.info['longName']
+            json_data = hist.to_json(orient='records', date_format='iso')
+            data_list = json.loads(json_data)
+
+            end_dict = {'info': {
+                'name': name,
+                'currency': currency
+            },
+                'data': data_list
+            }
+
+            response = JsonResponse(end_dict, status=status.HTTP_200_OK)
+            response['Access-Control-Allow-Origin'] = '*'
+            response['Access-Control-Allow-Headers'] = 'Content-Type, currency-pair, period, interval, ngrok-skip-browser-warning'
+
+            return response
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
